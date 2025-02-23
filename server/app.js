86b4e4d83f57905e35b4f4cd2e8188b6242c2ba4
zohaib <muhammadzohaibmalik10@gmail.com>
2025-02-23 21:59:29 +0500

    Add file renaming endpoint with CORS and JSON support

--- server/app.js
+++ server/app.js
@@ -1,41 +1,53 @@
 import express from 'express'
-
-import {readdir,rm} from 'fs/promises'
+import cors from 'cors'
+import { readdir, rename, rm } from 'fs/promises'
 
 const app = express()
+app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow frontend origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allow PATCH
+}))
 
-//Enable cors
-app.use((_, res, next) => {
-  res.set({
-    "Access-Control-Allow-Origin":"*",
-    "Access-Control-Allow-Methods":"*"
-  })
-  next()
-})  
+//Enable cors manually
+// app.use((_, res, next) => {
+//   res.set({
+//     "Access-Control-Allow-Origin": "*",
+//     "Access-Control-Allow-Methods": "*"
+//   })
+//   next()
+// })
 
-app.get("/:filename",(req,res)=>{
-  const {filename}=req.params
-  if(req.query.action==="download")
-  {
-    res.set('Content-Disposition','attachment')
+app.get("/:filename", (req, res) => {
+  const { filename } = req.params
+  if (req.query.action === "download") {
+    res.set('Content-Disposition', 'attachment')
   }
   res.sendFile(`${import.meta.dirname}/storage/${filename}`)
 })
 
-app.delete("/:filename",async(req,res)=>{
-  const {filename}=req.params
-  const filePath=`${import.meta.dirname}/storage/${filename}`
-  try{
-   await rm(filePath)
-    res.json({message:"file deleted successfully"})
-  }catch(err){
-    res.sendStatus(404).json({message:"file not found"})
+app.delete("/:filename", async (req, res) => {
+  const { filename } = req.params
+  const filePath = `${import.meta.dirname}/storage/${filename}`
+  try {
+    await rm(filePath)
+    res.json({ message: "file deleted successfully" })
+  } catch (err) {
+    res.sendStatus(404).json({ message: "file not found" })
   }
 })
 
+app.patch("/:filename", async (req, res) => {
+  const { filename } = req.params
+  const { newFileName } = req.body
+  await rename(`./storage/${filename}`,`./storage/${newFileName}`)
+      res.sendStatus(201).json({ message: "file rename successfully" })
+
+})
+
 // serving Dir
-app.get('/',async(_,res)=>{
-  const filesList=await  readdir('./storage')
+app.get('/', async (_, res) => {
+  const filesList = await readdir('./storage')
   res.json(filesList)
 })
 
