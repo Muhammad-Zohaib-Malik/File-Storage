b9d841d8618336dfa7c0e88cf3a99b885a162582
zohaib <muhammadzohaibmalik10@gmail.com>
2025-02-23 11:38:10 +0500

    Add file serving and CORS support to Express server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,27 @@
 import express from 'express'
+import {readdir} from 'fs/promises'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+//Enable cors
+app.use((_, res, next) => {
+  res.set("Access-Control-Allow-Origin","*")
+  next()
+})  
+
+app.get("/:filename",(req,res)=>{
+  const {filename}=req.params
+  if(req.query.action==="download")
+  {
+    res.set('Content-Disposition','attachment')
+  }
+  res.sendFile(`${import.meta.dirname}/storage/${filename}`)
+})
+
+// serving Dir
+app.get('/',async(_,res)=>{
+  const filesList=await  readdir('./storage')
+  res.json(filesList)
 })
 
 app.listen(4000, () => {
