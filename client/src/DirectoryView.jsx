15de2b461cfd6a5b2f00ea2cd22f51e2e4f85eaa
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-02 15:25:59 +0500

    Update server and client routes to fully support nested file and directory paths

--- client/src/DirectoryView.jsx
+++ client/src/DirectoryView.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export const DirectoryView = () => {
   const Base_URL = "http://192.168.0.103:4000";
@@ -7,6 +7,7 @@ export const DirectoryView = () => {
   const [progress, setProgress] = useState(0);
   const [newFilename, setNewFilename] = useState("");
   const { '*': dirPath } = useParams()
+  console.log(dirPath)
 
   async function getDirectoryItems() {
     const response = await fetch(`${Base_URL}/directory/${dirPath}`);
@@ -15,12 +16,12 @@ export const DirectoryView = () => {
   }
   useEffect(() => {
     getDirectoryItems();
-  }, []);
+  }, [dirPath]);
 
   async function uploadFile(e) {
     const file = e.target.files[0];
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", `${Base_URL}/files/${file.name}`, true);
+    xhr.open("POST", `${Base_URL}/files/${dirPath}/${file.name}`, true);
     xhr.setRequestHeader("filename", file.name);
     xhr.addEventListener("load", () => {
       console.log(xhr.response);
@@ -34,7 +35,7 @@ export const DirectoryView = () => {
   }
 
   async function handleDelete(filename) {
-    const response = await fetch(`${Base_URL}/files/${filename}`, {
+    const response = await fetch(`${Base_URL}/files/${dirPath}/${filename}`, {
       method: "DELETE",
     });
     const data = await response.text();
@@ -49,12 +50,12 @@ export const DirectoryView = () => {
 
   async function saveFilename(oldFilename) {
     setNewFilename(oldFilename);
-    const response = await fetch(`${Base_URL}/files/${oldFilename}`, {
+    const response = await fetch(`${Base_URL}/files/${dirPath}/${oldFilename}`, {
       method: "PATCH",
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ newFilename }),
+      body: JSON.stringify({ newFilename: `${dirPath}/${newFilename}` }),
     });
     const data = await response.text();
     console.log(data);
@@ -75,7 +76,7 @@ export const DirectoryView = () => {
       {directoryItems.map(({ name, isDirectory }, i) => (
         <div key={i}>
           {name}  {
-            isDirectory && <a href={`./${name}`}>Open</a>
+            isDirectory && <Link to={`./${name}`}>Open</Link>
           }
           {
             !isDirectory && <a href={`${Base_URL}/files/${dirPath}/${name}?action=open`}>Open</a>
--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ import { readdir, rename, rm, stat } from 'fs/promises'
 
 const app = express()
 app.use(express.json())
+
 app.use(cors())
+
 
 // app.get("/files/:filename", (req, res) => {
 //   const { filename } = req.params
@@ -24,35 +26,34 @@ app.use(cors())
     res.sendFile(`${import.meta.dirname}/storage/${filePath}`)
   })
   
-app.post('/files/:filename', async (req, res) => {
-  const writeStream = await createWriteStream(`./storage/${req.params.filename}`)
+app.post('/files/*', async (req, res) => {
+  const writeStream = await createWriteStream(`./storage/${req.params[0]}`)
   req.pipe(writeStream)
   req.on("end", () => {
     res.json({ message: "File uploaded" })
   })
 })
 
-app.delete("/files/:filename", async (req, res) => {
-  const { filename } = req.params
-  const filePath = `${import.meta.dirname}/storage/${filename}`
+app.delete("/files/*", async (req, res) => {
+  const { 0:filePath } = req.params
+  const fileDelete = `${import.meta.dirname}/storage/${filePath}`
   try {
-    await rm(filePath)
+    await rm(fileDelete,{recursive:true})
     res.json({ message: "file deleted successfully" })
   } catch (err) {
     res.sendStatus(404).json({ message: "file not found" })
   }
 })
 
-app.patch("/files/:filename", async (req, res) => {
-  const { filename } = req.params
-  const { newFileName } = req.body
-  await rename(`./storage/${filename}`, `./storage/${newFileName}`)
+app.patch("/files/*", async (req, res) => {
+  const { 0:filePath } = req.params
+  await rename(`./storage/${filePath}`, `./storage/${req.body.newFilename}`)
   res.json({ message: "Renamed" });
 })
 
 // serving Dir
-app.get('/directory/:dirname', async (req, res) => {
-  const { dirname } = req.params
+app.get('/directory/?*', async (req, res) => {
+  const {0:dirname}=req.params
   // console.log(dirname)
   const fullDirPath = `./storage/${dirname ? dirname : ''}`
   const filesList = await readdir(fullDirPath)
