0f21806d3d91163a4d82e788ec15aedc84cfdd64
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-19 09:52:18 +0500

    Refactor database connection handling by adding a disconnect function and ensuring graceful termination on process exit. Improve code readability by standardizing formatting.

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const uri = "mongodb://127.0.0.1:27017/storageApp";
-let client; 
+let client;
 
 export const connectToDatabase = async () => {
   try {
@@ -10,18 +10,22 @@ export const connectToDatabase = async () => {
       await client.connect();
       console.log(`Connected to database ${uri}`);
     }
-    return client.db(); 
+    return client.db();
   } catch (error) {
     console.error("Error connecting to database:", error);
-    throw error; 
+    throw error;
+  }
+};
+
+export const disconnectFromDatabase = async () => {
+  if (client) {
+    await client.close();
+    console.log("Database connection closed.");
   }
 };
 
 // Handle process termination gracefully
 process.on("SIGINT", async () => {
-  if (client) {
-    await client.close();
-    console.log("Database connection closed.");
-  }
+  await disconnectFromDatabase();
   process.exit(0);
 });
