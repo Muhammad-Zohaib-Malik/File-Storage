8085143e0a3d351ba839a11d2acd997a5ca20ccf
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-21 15:58:43 +0500

    add lean

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,4 +1,3 @@
-import { ObjectId } from "mongodb";
 import { User } from "../models/user.model.js";
 
 export const checkAuth = async (req, res, next) => {
@@ -7,7 +6,7 @@ export const checkAuth = async (req, res, next) => {
   if (!uid) {
     return res.status(401).json({ error: "Not Logges In" });
   }
-  const user = await User.findOne({ _id: new ObjectId(uid) });
+  const user = await User.findOne({ _id: uid }).lean();
   console.log(user);
   if (!user) {
     return res.status(401).json({ error: "Not User Found" });
