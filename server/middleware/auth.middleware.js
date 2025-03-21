d53b374242bade0553ec8ac41c3259c40f99e70f
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-21 14:39:45 +0500

    added mongoose

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,13 +1,14 @@
 import { ObjectId } from "mongodb";
+import { User } from "../models/user.model.js";
 
 export const checkAuth = async (req, res, next) => {
   const { uid } = req.cookies;
-  const db = req.db;
 
   if (!uid) {
     return res.status(401).json({ error: "Not Logges In" });
   }
-  const user = await db.collection("users").findOne({ _id: new ObjectId(uid) });
+  const user = await User.findOne({ _id: new ObjectId(uid) });
+  console.log(user);
   if (!user) {
     return res.status(401).json({ error: "Not User Found" });
   }
