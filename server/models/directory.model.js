39a329beced6424cd76c2723ec3098a3aa6d5c84
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-21 14:13:10 +0500

    setup model for directory

---
+++ server/models/directory.model.js
@@ -0,0 +1,24 @@
+import { model, Schema } from "mongoose";
+
+const directorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "name is required"],
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User Id is required"],
+    },
+    parentDirId: {
+      type: Schema.Types.ObjectId,
+      ref: "Directory",
+      default: null,
+    },
+  },
+  { timestamps: true, strict: "throw" }
+);
+
+export const Directory = model("Directory", directorySchema);
