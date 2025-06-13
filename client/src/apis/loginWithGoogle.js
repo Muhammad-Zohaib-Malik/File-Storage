2b70bcaaae6da873adbeef3a0ed4cad8769aef9c
zohaib <muhammadzohaibmalik10@gmail.com>
2025-06-13 20:24:54 +0500

    add axios for API requests and implement user authentication functions

--- client/src/apis/loginWithGoogle.js
+++ client/src/apis/loginWithGoogle.js
@@ -1,17 +1,6 @@
+import { axiosWithoutCreds } from "./axiosInstances";
+
 export const loginWithGoogle = async (idToken) => {
-  const BASE_URL = "http://localhost:4000";
-  try {
-    const response = await fetch(`${BASE_URL}/user/google`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ idToken }),
-      credentials: "include",
-    });
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error logging in with Google:", error);
-  }
+  const { data } = await axiosWithoutCreds.post("/user/google", { idToken });
+  return data;
 };
