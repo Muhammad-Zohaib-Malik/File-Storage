d7e5bc3845b0251d0f0d34cb0005bff88c1f4e39
zohaib <muhammadzohaibmalik10@gmail.com>
2025-06-17 22:52:38 +0500

    feat: add Toaster component for improved notification display

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Toaster } from "react-hot-toast";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
       <App />
+      <Toaster position="top-right" reverseOrder={false} />
     </GoogleOAuthProvider>
-  </StrictMode>,
+  </StrictMode>
 );
