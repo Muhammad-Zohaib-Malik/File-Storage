7517b0c043c0ae01c1952b850569deec5de5f31e
zohaib <muhammadzohaibmalik10@gmail.com>
2025-08-13 21:26:57 +0500

    fix: remove unused driveRoutes file

--- server/routes/driveRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { importFromDrive } from "../controllers/fileController.js";
-
-const router = express.Router();
-
-router.post("/import-from-drive", importFromDrive);
-
-export default router;
