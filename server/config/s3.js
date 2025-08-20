a2df6269e88e2fc813fedf80df7e84e0ed817e81
zohaib <muhammadzohaibmalik10@gmail.com>
2025-08-20 14:32:51 +0500

    feat: implement S3 file operations with signed URL generation for uploads and downloads

---
+++ server/config/s3.js
@@ -0,0 +1,59 @@
+import {
+  PutObjectCommand,
+  S3Client,
+  GetObjectCommand,
+  HeadObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3Client = new S3Client({
+  profile: "nodejs",
+});
+
+export const createUploadSignedUrl = async ({ key, ContentType }) => {
+  const command = new PutObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: key,
+    ContentType: ContentType,
+  });
+
+  const url = await getSignedUrl(s3Client, command, {
+    expiresIn: 3600,
+    signableHeaders: new Set(["Content-type"]),
+  });
+  return url;
+};
+
+export const createGetSignedUrl = async ({
+  key,
+  download = false,
+  filename,
+}) => {
+  const command = new GetObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: key,
+    ResponseContentDisposition: `${download ? "attachment" : "inline"}; filename=${encodeURIComponent(filename)}`,
+  });
+
+  const url = await getSignedUrl(s3Client, command, {
+    expiresIn: 300,
+  });
+  return url;
+};
+
+export const getS3FileMetaData = async ({ key }) => {
+  const command = new HeadObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: key,
+  });
+  return await s3Client.send(command);
+};
+
+export const deleteS3FileFromAws = async ({ key }) => {
+  const command = new DeleteObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: key,
+  });
+  await s3Client.send(command);
+};
