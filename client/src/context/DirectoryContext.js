ee218c8614c2a78cd86aa32fa58cd53b5506dec6
zohaib <muhammadzohaibmalik10@gmail.com>
2025-08-26 16:09:55 +0500

    feat: add handleOpenShareModal to DirectoryContext for share modal functionality

--- client/src/context/DirectoryContext.js
+++ client/src/context/DirectoryContext.js
@@ -1,7 +1,16 @@
 import { createContext, useContext } from "react";
 
-export const DirectoryContext = createContext(null);
+export const DirectoryContext = createContext({
+  handleOpenShareModal: () => {},
+  // Other existing context values
+});
 
 export function useDirectoryContext() {
-  return useContext(DirectoryContext);
+  const context = useContext(DirectoryContext);
+  if (context === undefined) {
+    throw new Error(
+      "useDirectoryContext must be used within a DirectoryProvider"
+    );
+  }
+  return context;
 }
