9da811446ce0ccdf75f909e115bee2f56c68bcc6
zohaib <muhammadzohaibmalik10@gmail.com>
2025-12-21 16:38:46 +0500

    fix: update CORS origin to use environment variable for better configuration

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(helmet());
 // app.use(createRateLimiter());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
