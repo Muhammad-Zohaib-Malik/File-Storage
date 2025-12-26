3bba9870230652d3fbf2bf163301eff7352b924b
zohaib <muhammadzohaibmalik10@gmail.com>
2025-12-26 10:57:36 +0500

    feat: add dummyTest.js for basic test execution and update package.json to include test script

---
+++ client/dummyTest.js
@@ -0,0 +1,12 @@
+const shouldTestPass=true
+
+if(shouldTestPass){
+    console.log("Test Passed")
+    // eslint-disable-next-line no-undef
+    process.exit(0)
+}
+else {
+    console.log("Test Failed")
+    // eslint-disable-next-line no-undef
+    process.exit(1)
+}
